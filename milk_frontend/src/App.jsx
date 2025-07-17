bad752d66b132287cf71ed42e10d37ffcc427ee9
Vedant Bhat <vedusbat9@gmail.com>
2025-07-17 23:22:17 +0530

    vedant and sush merged code

--- milk_frontend/src/App.jsx
+++ milk_frontend/src/App.jsx
@@ -5,19 +5,35 @@ import "./App.css";
 import Sidebar, { SidebarItem } from "./components/SideBar";
 import { LayoutDashboard } from "lucide-react";
 import { FileText, Receipt, Truck, Inbox, Mail } from "lucide-react";
+import Stat_card from './components/Stat_card'
+import { useContext } from 'react';
+import Data_table_state from './context/DataTableState';
 
 function App() {
   const [count, setCount] = useState(0);
+  const arr = [1, 2, 3, 4];
 
   return (
-    <main className="w-fit">
-      <Sidebar>
-        <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" />
-        <SidebarItem icon={<Truck size={20} />} text="Delivery" />
-        <SidebarItem icon={<Inbox size={20} />} text="Requests" />
-        <SidebarItem icon={<Receipt size={20} />} text="Bill" />
-      </Sidebar>
-    </main>
+    <Data_table_state>
+      <main className="w-full flex ">
+        <Sidebar>
+          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" />
+          <SidebarItem icon={<Truck size={20} />} text="Delivery" />
+          <SidebarItem icon={<Inbox size={20} />} text="Requests" />
+          <SidebarItem icon={<Receipt size={20} />} text="Bill" />
+        </Sidebar>
+        
+        <div className="container" style={{display : 'flex' , flexDirection : 'column'}}>
+          <div className="main">
+            {
+              arr.map((item) => {
+                return <Stat_card />
+              })
+            }
+          </div>
+        </div>
+      </main>
+    </Data_table_state>
   );
 }
 
